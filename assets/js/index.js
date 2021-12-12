33b077437acc148adfe088c01a39528379b39423
Dylan <duyulun@rcrai.com>
2021-12-12 13:37:41 +0800

    update maps part

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,20 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
+// Function to toggle the bar
+function togglefunction() {
+    var x = document.getElementById("menus");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+// Function to toggle the plus menu into minus
+function myFunction(x) {
+    x.classList.toggle("fa fa-bars");
+}
